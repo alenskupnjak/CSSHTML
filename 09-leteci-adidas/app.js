5c7dfdc9f3504405dac1f17ab62259c2507e1b96
AlenS <alenskupnjak@yahoo.com>
2021-01-09 10:39:03 +0100

    076 razno

--- 09-leteci-adidas/app.js
+++ 09-leteci-adidas/app.js
@@ -1,6 +1,5 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
-//Items
 const title = document.querySelector(".title");
 const sneaker = document.querySelector(".sneaker img");
 const purchase = document.querySelector(".purchase");
@@ -9,7 +8,7 @@ const sizes = document.querySelector(".sizes");
 
 
 
-//Moving Animation Event
+// Moving Animation Event
 container.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
@@ -18,7 +17,7 @@ container.addEventListener("mousemove", (e) => {
 
 // Animate In
 container.addEventListener('mouseenter', e => {
-  card.style.transition = 'none'
+  card.style.transition = 'all 500ms ease'
   title.style.transform = 'translateZ(100px)'
   sneaker.style.transform = 'translateZ(50px) rotateZ(-30deg)'
   purchase.style.transform = 'translateZ(70px)'
@@ -29,7 +28,7 @@ container.addEventListener('mouseenter', e => {
 
 // Animete Out
 container.addEventListener('mouseleave', e=> {
-  card.style.transition = 'all 500ms ease'
+  card.style.transition = 'all 1500ms ease'
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   title.style.transform = 'translateZ(0)'
   sneaker.style.transform = 'translateZ(0) rotateZ(0)'
